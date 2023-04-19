65b0e8576d95829dc8f688d2b73c9d7189cbe4c0
rvanxer <rajatverma1995@gmail.com>
2023-04-19 15:14:06 -0400

    Updated notebooks, removed figures and tex folder

--- code/setup.py
+++ code/setup.py
@@ -1,37 +1,67 @@
 import sys
 sys.path.insert(0, '/home/umni2/a/umnilab/users/verma99/mk/mobilkit')
-from mobilkit.umni import *
+import mobilkit.utils as U
 
-P = Project('..')
+P = U.Project('..')
 
 class Region:
     """ A simple class for handling study regions (mostly MSAs). """
-
-    def __init__(self, name: str, state: str, counties: list[str]):
+    def __init__(self, name: str, state: str, counties: list[str], 
+                 geocode=None, root=P.root / 'regions'):
         self.name = name
-        self.label = name.replace(' ', '_').lower()
         self.state = state
+        self.root = root
         self.counties = sorted(counties)
-        # folder containing this region's data
-        self.data = P.data / str(self)
-        # folder containing this region's geometry data
-        self.geom = self.data / 'geometry'
-        # add its details to the params file
-        P.params.set({'regions': {str(self): {
-            'state': state, 'counties': self.counties}}})
+        self.key = name.replace(' ', '_').lower()
+        self.geocode = geocode or f'{name}, {state}'
+        self.data = U.mkdir(root / self.key)
+        self.datasets = set()
+        P.params.set({'regions': {self.key: {
+            'name': name, 'state': state, 'counties': self.counties}}})
 
     def __repr__(self):
         return f'Region({self.name})'
     
-    def __str__(self):
-        return self.label
+    def __hash__(self):
+        return hash(self.geocode)
+    
+    def __eq__(self, other):
+        return self.geocode == other.geocode
     
     @staticmethod
-    def load(name, state=None):
-        rgn = Region(name, state, counties=[])
-        rgn.cnty = gpd.read_file(rgn.geom / 'counties.gpkg')
-        rgn.counties = sorted(list(rgn.cnty['name']))
-        rgn.bbox = list(rgn.cnty.geometry.unary_union.bounds)
-        P.params.set({'regions': {str(rgn): {
-            'counties': rgn.counties, 'bbox': rgn.bbox}}})
+    def load(key):
+        key = key.replace(' ', '_').lower()
+        x = P.params.get(f'regions.{key}')
+        rgn = Region(x['name'], x['state'], x['counties'])
+        rgn.bbox = P.params.get(f'regions.{rgn.key}.bbox')
         return rgn
+
+
+class Dataset:
+    """ A container of a combination of region and analysis period. """
+    def __init__(self, key, region, start_date, end_date,
+                 root=P.root / 'datasets'):
+        self.key = key
+        if isinstance(region, str):
+            region = Region.load(region)
+        self.region = region
+        self.start = U.to_date(start_date)
+        self.end = U.to_date(end_date)
+        self.dates = U.dates(self.start, self.end)
+        self.data = U.mkdir(root / key)
+        region.datasets.add(self)
+        P.params.set({'datasets': {key: {
+            'region': region.key, 'start': str(self.start),
+            'end': str(self.end)}}})
+        
+    def fmt_dates(self, sep=' - ', fmt='%Y-%m-%d'):
+        return sep.join([d.strftime(fmt) for d in [self.start, self.end]])
+        
+    def __repr__(self):
+        return f'Dataset {self.key}({self.region.name}: {self.fmt_dates()})'
+
+    @staticmethod
+    def load(key, region=None):
+        x = P.params.get(f'datasets.{key}')
+        rgn = region or Region.load(x['region'])
+        return Dataset(key, rgn, x['start_date'], x['end_date'])
